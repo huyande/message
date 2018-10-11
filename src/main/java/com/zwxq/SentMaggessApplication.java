79cc673f11f1e1c7e3ab562fa8365aadaa3bf24e
huyande <1473280483@qq.com>
2018-10-11 14:57:51 +0800

    提交文件

---
+++ src/main/java/com/zwxq/SentMaggessApplication.java
@@ -0,0 +1,14 @@
+package com.zwxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class SentMaggessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentMaggessApplication.class, args);
+	}
+}
